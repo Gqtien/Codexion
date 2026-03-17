7d2324f3ccd5cc925c3184b6210df2ab16ac4c90
Gqtien <bin.gatien@gmail.com>
2026-03-17 22:49:45 +0100

    feat: custom usleep

--- src/utils/time.c
+++ src/utils/time.c
@@ -1,8 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   time.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gviola-l <gviola-l@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/17 17:38:50 by gviola-l          #+#    #+#             */
+/*   Updated: 2026/03/17 17:38:51 by gviola-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "codexion.h"
 
-unsigned long get_time_ms()
+unsigned long	get_time_ms(void)
 {
-    struct timeval tv;
-    gettimeofday(&tv, NULL);
-    return (tv.tv_sec * 1000UL) + (tv.tv_usec / 1000UL);
+	struct timeval	tv;
+
+	gettimeofday(&tv, NULL);
+	return ((tv.tv_sec * 1000UL) + (tv.tv_usec / 1000UL));
+}
+
+void	ft_usleep(unsigned long ms, t_data *data)
+{
+	unsigned long	start;
+
+	start = get_time_ms();
+	while (get_time_ms() - start < ms)
+	{
+		if (!is_running(data))
+			break ;
+		usleep(500);
+	}
 }
